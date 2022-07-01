17e43aaec5b4565802bf6d3fb976755bbfc22774
oregonjoe <joe@chetcodigital.com>
2022-07-01 13:59:57 -0700

    stats update

--- web/static/plugins/helmsmart.widgets.js
+++ web/static/plugins/helmsmart.widgets.js
@@ -3209,11 +3209,13 @@
 					var myzoom = map.getZoom();
 					map.fitBounds(bounds);
 				
-					map.setZoom(8);
+					map.setZoom(myzoom);
 				}
 				catch(err) {
 					console.log("error object toString():");
 					console.log("\t" + err.toString());
+					
+					map.setZoom(8);
 				};
 				
 				
