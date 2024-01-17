2b6de0371bba5a2ad6da89e8823d039d732fe00a
oregonjoe <joe@chetcodigital.com>
2024-01-17 14:16:28 -0800

    api logging

--- web/app.py
+++ web/app.py
@@ -1571,7 +1571,21 @@ def get_apistat_all():
       log.info('get_apistat:  jsondatasorted deviceid %s:%s:%s ', apitags['apikey'],  apitags['deviceid'],  apitags['devicename'],  )         
       log.info('get_apistat:  jsondatasorted apitotals %s: ', apitotals)
 
-    """      
+    """
+
+ 
+    for key in jsondatagrouped:
+      log.info('get_apistat:  jsondatasorted email %s: ', key)
+      apivalues = jsondatagrouped[key]
+      apitotals = 0
+      for apitags in apivalues:
+        #apitotals = apitotals + int(apitags['apidata'] )
+        #log.info('get_apistat:  jsondatasorted apifunction %s: %s ', apitags['apifunction'], apitags['apidata'] )
+        log.info('get_apistat:  jsondatasorted apitags %s: ', apitags )
+        
+      #log.info('get_apistat:  jsondatasorted deviceid %s:%s:%s ', apitags['apikey'],  apitags['deviceid'],  apitags['devicename'],  )         
+      #log.info('get_apistat:  jsondatasorted apitotals %s: ', apitotals)
+  
     #list_length = len(jsondatagrouped)
     #for i in range(list_length):
 
