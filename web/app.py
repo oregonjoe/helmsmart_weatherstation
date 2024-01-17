381cb6481ac72c45cdc31a739ce643851f908ec5
oregonjoe <joe@chetcodigital.com>
2024-01-17 13:39:46 -0800

    api logging

--- web/app.py
+++ web/app.py
@@ -1545,7 +1545,8 @@ def get_apistat_all():
       else:
         jsonvaluestr = '{ "deviceid":"' + elem['deviceid'] + '","devicename":"' + elem['devicename'] + '",  "apifunction":"' +elem['apifunction'] + '","apidata":"'+ str(elem['value'])+ '"}'
         log.info('get_apistat:  jsonvaluestr %s:  ', jsonvaluestr)
-        jsondatagrouped[elem['useremail']][ elem['apikey']]=json.loads(jsonvaluestr)
+        #jsondatagrouped[elem['useremail']][ elem['apikey']]=json.loads(jsonvaluestr)
+        jsondatagrouped[elem['useremail']][ elem['apikey']]=jsonvaluestr
         #jsondatagrouped[elem['useremail']][ elem['apikey']]=json.loads("{ 'deviceid':elem['deviceid'],'devicename':elem['devicename'],  'apifunction':elem['apifunction'],'apidata':elem['value']}")
         #jsondatagrouped[elem['useremail']][ elem['apikey']]=[elem['apifunction']]
 
